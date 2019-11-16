9640a49a68c2a417fa9bd1b82076082bfe0c20a4
pinho <lucashpsfei@gmail.com>
2019-11-16 16:23:57 -0300

    Segregando as telas dos formulários, pendente o envio para api

--- app/src/main/java/com/fei/arnutri/FormNutrientesActivity.java
+++ app/src/main/java/com/fei/arnutri/FormNutrientesActivity.java
@@ -7,25 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class FormularioActivity extends AppCompatActivity {
-
+public class FormNutrientesActivity extends AppCompatActivity {
     private Button btnProx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario);
+        setContentView(R.layout.activity_form_nutrientes);
 
-        btnProx = findViewById(R.id.btnProx1);
+        btnProx = findViewById(R.id.btnProx3);
 
         btnProx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FormularioActivity.this, FormPessoalActivity.class);
+                Intent intent = new Intent(FormNutrientesActivity.this, FormGordurasActivity.class);
                 startActivity(intent);
             }
         });
-
-
     }
 }
